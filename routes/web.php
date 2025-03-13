0e460ce93a680566d86816f5f226c9db736f15cb
faishal-ai <okbos6337@gmail.com>
2025-03-13 19:30:44 +0700

    j3 Praktikum-5

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
-
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\LevelController;
+// use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 // Route::get('/hello', function () {
 //     return 'World';
