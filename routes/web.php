76791333442f6590bcaf10882b94df64b6a4bf64
faishal-ai <okbos6337@gmail.com>
2025-03-13 20:13:28 +0700

    j3 Praktikum-6

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
+
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\LevelController;
-// use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +11,21 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+
+
+// use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\LevelController;
+// use Illuminate\Support\Facades\Route;
+// // use App\Http\Controllers\LevelController;
+// // use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
 
 // Route::get('/hello', function () {
 //     return 'World';
