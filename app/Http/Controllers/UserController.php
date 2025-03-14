455f91e88dafb4c05bc01cc9bbbc276753305bea
faishal-ai <okbos6337@gmail.com>
2025-03-14 21:46:02 +0700

    js4 praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,15 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
+    public function index(){
+        $user = UserModel::where('level_id', 2)->count();
+        return view('user', ['data' => $user]);
+
         // $user = UserModel::findOr(20, ['username', 'nama'], function () {
         //     abort(404);
         // });
-        $user = UserModel::where('username', 'manager9')->findOrFail();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::where('username', 'manager9')->findOrFail();
+        // return view('user', ['data' => $user]);
         
         
         // $user = UserModel::firstWhere('level_id', 1);
