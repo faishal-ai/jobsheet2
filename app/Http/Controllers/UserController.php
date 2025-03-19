b5a2a1426981628bbda9a8d7e24ce1c3c49d8c35
faishal-ai <okbos6337@gmail.com>
2025-03-19 22:13:00 +0700

    j5 praktikum4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,18 +22,26 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // Menentukan menu aktif
+        $level = LevelModel::all();
 
         return view('user.index', [
             'breadcrumb' => $breadcrumb,
             'page' => $page,
+            'level' => $level,
             'activeMenu' => $activeMenu
         ]);
     }
 
     public function list(Request $request) 
     { 
-        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level'); 
-     
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
+        ->with('level'); 
+        
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
         return DataTables::of($users) 
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
             ->addIndexColumn()  
